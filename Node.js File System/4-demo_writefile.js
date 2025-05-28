df59ef72338fab747df7502b04c10df19ed56cad
mzeeshan138 <muhammadzeeshan786138@gmail.com>
2025-05-28 11:18:37 +0500

    read file

--- Node.js File System/4-demo_writefile.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
-  if (err) throw err;
-  console.log('Replaced!');
-});
--- Node.js File System/5-demo_deletefile.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-fs.unlink('mynewfile2.txt', function (err) {
-  if (err) throw err;
-  console.log('File deleted!');
-});
