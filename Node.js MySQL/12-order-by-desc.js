89c0f98473d380d85ef871b678a2646957800523
mzeeshan138 <muhammadzeeshan786138@gmail.com>
2025-06-03 16:53:52 +0500

    12-order-by-desc

---
+++ Node.js MySQL/12-order-by-desc.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'mydb',
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log('Database is Connected!');
+
+  con.query(
+    'SELECT * FROM customers ORDER BY name DESC',
+    function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    }
+  );
+});
