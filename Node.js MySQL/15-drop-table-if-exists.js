0e4b3794173f27d6789729f50deab3fe8753c0d1
mzeeshan138 <muhammadzeeshan786138@gmail.com>
2025-06-04 09:47:29 +0500

    15-drop-table-if-exists

---
+++ Node.js MySQL/15-drop-table-if-exists.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'mydb',
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log('Database is Connected!');
+
+  const sql = 'DROP TABLE IF EXISTS customers';
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
