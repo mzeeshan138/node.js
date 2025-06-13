031700b35ebe5e2f1e5b0f350def45294253d357
mzeeshan138 <muhammadzeeshan786138@gmail.com>
2025-06-13 15:34:32 +0500

    full join

---
+++ Node.js MySQL/21-full-join.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'mydb',
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log('Database is Connected!');
+
+  const sql =
+    'SELECT * FROM users LEFT JOIN products ON users.favorite_product = products.id UNION SELECT * FROM users RIGHT JOIN products ON users.favorite_product = products.id';
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
+
+//inner --> SELECT * FROM users JOIN products ON users.favorite_product = products.id;
+//inner --> SELECT * FROM users INNER JOIN products ON users.favorite_product = products.id;
+//left --> SELECT * FROM users LEFT JOIN products ON users.favorite_product = products.id;
+//right --> SELECT * FROM users RIGHT JOIN products ON users.favorite_product = products.id;
+//full --> SELECT * FROM users LEFT JOIN products ON users.favorite_product = products.id UNION SELECT * FROM users RIGHT JOIN products ON users.favorite_product = products.id;
